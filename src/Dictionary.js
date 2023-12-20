fa4de97d3dbc08ba6d59d77c92ff81e719264a23
eugesuri <140730354+eugesuri@users.noreply.github.com>
2023-12-20 10:46:40 +0100

    added input

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,13 @@
 import React from "react";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-  return "Hello ";
+  return (
+    <div className="Dictionary">
+      {" "}
+      <form>
+        <input type="Search" />
+      </form>{" "}
+    </div>
+  );
 }
