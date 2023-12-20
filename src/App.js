a6b3d0aa2bb8b534dd1008f18ed0842b0502bb77
eugesuri <140730354+eugesuri@users.noreply.github.com>
2023-12-20 10:30:07 +0100

    added footer and links

--- src/App.js
+++ src/App.js
@@ -4,9 +4,31 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />{" "}
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />{" "}
+        </header>
+        <footer className="text-center">
+          {" "}
+          Coded by Eugenia Brunetti Ehnimb and is open-sourced on {""}
+          <a
+            href="https://github.com/eugesuri/dictionary-project"
+            title="Eugenia Brunetti Github"
+            target="_blank"
+          >
+            Github
+          </a>{" "}
+          and hosted on {""}
+          <a
+            href="https://app.netlify.com/sites/dictionary-app-eugenia-brunetti-e/overview"
+            title="Eugenia Brunetti netlify"
+            target="_blank"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
